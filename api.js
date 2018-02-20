84d25312a17125ebdeb17e6687abcbe89ebe7328
KristjanP <kth130@hi.is>
2018-02-20 22:56:14 +0000

    routes sett

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const xss = require('xss');
 
 const {
   create,
@@ -15,5 +16,55 @@ function catchErrors(fn) {
 }
 
 /* todo útfæra api */
+router.get('/', async (req, res) => {
+  const json = await readAll();
+  console.info(json);
+  res.send(json);
+});
+
+router.post('/', (req, res) => {
+  const {
+    title = '',
+    text = '',
+    datetime = '',
+  } = req.body;
+
+  const note = {
+    title: xss(title),
+    text: xss(text),
+    datetime: xss(datetime),
+  };
+  create(note);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  console.info(id);
+  const json = await readOne(id);
+  console.info(json);
+  res.send(json);
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const {
+    title = '',
+    text = '',
+    datetime = '',
+  } = req.body;
+
+  const note = {
+    title: xss(title),
+    text: xss(text),
+    datetime: xss(datetime),
+  };
+  update(id, note);
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params;
+
+  del(id);
+});
 
 module.exports = router;
